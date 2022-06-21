0d7c4fe49e2acf9a6cba0aa465ce244d7309e313
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-22 04:04:20 +0600

    Accessing the API using API key and parameters.

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+api_key = "dd121110304b3377d428a8d4cea4fe5b"
+
+
+parameters = {
+    "lat": 23.810331,
+    "lon": 90.412521,
+    "appid": api_key
+}
+respond = requests.get("https://api.openweathermap.org/data/2.5/onecall", params=parameters)
+
+data = respond.json()
+print(data)
