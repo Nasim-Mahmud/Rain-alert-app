3c367519446cbdd1871bd38487e5cbfea6ee1a1e
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-22 04:32:37 +0600

    Accessing the API using API key and parameters.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import requests
+from twilio.rest import Client
+
 
 api_key = "dd121110304b3377d428a8d4cea4fe5b"
-
+OWN_API = "https://api.openweathermap.org/data/2.5/onecall"
+account_sid = "ACc65f7f168ddb0c92bbc38453d0e57978"
+auth_token = "e24ecdf8af2167e03dc5e49abdaf817e"
 
 parameters = {
     "lat": 23.810331,
     "lon": 90.412521,
     "appid": api_key
 }
-respond = requests.get("https://api.openweathermap.org/data/2.5/onecall", params=parameters)
+respond = requests.get(OWN_API, params=parameters)
 respond.raise_for_status()
 
 data = respond.json()
